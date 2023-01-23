5bb392faec5f6a66f45bfdd1c5a37fc6312dd23c
sayakg <sayakghosh@gofynd.com>
2023-01-23 12:36:48 +0530

    change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My name is TateG
+          My name is TateG Yes
         </p>
         <a
           className="App-link"
